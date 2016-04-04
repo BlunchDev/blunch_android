18381a42f2bcd8941cff41fc57da8eb350d9d74f
Víctor <victor.perez.martos@est.fib.upc.edu>
2016-04-04 21:14:16 +0200

    Interface work

--- app/src/main/java/dev/blunch/blunch/CollaborativeMenuAnswer.java
+++ app/src/main/java/dev/blunch/blunch/CollaborativeMenuAnswer.java
@@ -3,6 +3,10 @@ package dev.blunch.blunch;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import java.util.Vector;
 
 public class CollaborativeMenuAnswer extends AppCompatActivity {
 
@@ -12,6 +16,23 @@ public class CollaborativeMenuAnswer extends AppCompatActivity {
         setContentView(R.layout.activity_collaborative_menu_answer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fillHostSuggestions();
+    }
+
+    private void fillHostSuggestions(){
+        /*
+        Accedir a BD i consultar les recomanacions del host o pasarles des de la
+        pantalla anterior
+         */
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.CollaborativeMenuAnswerHostSuggestions);
+        Vector<String> hostSuggestions = new Vector<String>();
+        for(int i = 0; i < hostSuggestions.size();++i){
+            CheckBox checkBox = new CheckBox(getApplicationContext());
+            checkBox.setText(hostSuggestions.get(i));
+            linearLayout.addView(checkBox);
+        }
+
     }
 
 }
