032db65824ccab0b685aa9ddb9256b929afba1d2
Daniela Santos <fc44295@alunos.fc.ul.pt>
2016-04-04 21:17:51 +0200

    Creacion de la class Menu

---
+++ app/src/main/java/dev/blunch/blunch/Dish.java
@@ -0,0 +1,30 @@
+package dev.blunch.blunch;
+
+/**
+ * Created by Daniela Santos on 01-04-2016.
+ */
+public class Dish {
+    private String name;
+    private float price;
+
+    public Dish(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
--- app/src/main/java/dev/blunch/blunch/payMenu.java
+++ app/src/main/java/dev/blunch/blunch/payMenu.java
@@ -7,17 +7,25 @@ import java.sql.Date;
  */
 public class payMenu  extends Menu{
 
-    private float price;
+    private float dishPrice;
+    private Dish[] listDishs;
 
     public payMenu(String nameAuth, String nameMenu, String type, Date createDate, String local) {
         super(nameAuth, nameMenu, type, createDate, local);
-        if(checkPrice(price)){
-            this.price = price;
+        if(checkPrice(dishPrice)){
+            this.dishPrice = dishPrice;
         }
     }
 
-    private boolean checkPrice(float price){
-        return (price <= 0); //se el precio no es valido retuno false
+    private boolean checkPrice(float dishPrice){
+        return (dishPrice <= 0); //se el precio no es valido retuno false
     }
 
+    private float totalPrice(Dish[] listDishs){
+        int sum = 0;
+        for(int i = 0; i < listDishs.length; i++ ){
+            sum += listDishs[i].getPrice();
+        }
+        return sum;
+    }
 }
