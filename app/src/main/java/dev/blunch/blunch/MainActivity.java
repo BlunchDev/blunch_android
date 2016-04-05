358f6b93bc4148f51fb643b3bfcc0246b55404cc
Víctor <victor.perez.martos@est.fib.upc.edu>
2016-04-05 14:00:25 +0200

    some random stuff

--- app/src/main/java/dev/blunch/blunch/MainActivity.java
+++ app/src/main/java/dev/blunch/blunch/MainActivity.java
@@ -1,5 +1,6 @@
 package dev.blunch.blunch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent i = new Intent( MainActivity.this, CollaborativeMenuAnswer.class);
+        startActivity(i);
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
