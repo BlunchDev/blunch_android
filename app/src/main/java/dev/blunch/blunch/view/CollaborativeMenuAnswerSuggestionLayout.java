75b3a49ff182cc963b3e85de109d83ebe5ef8a79
albert <albert.suarez.molgo@est.fib.upc.edu>
2016-04-05 22:58:05 +0200

    change package path

--- app/src/main/java/dev/blunch/blunch/view/CollaborativeMenuAnswerSuggestionLayout.java
+++ app/src/main/java/dev/blunch/blunch/view/CollaborativeMenuAnswerSuggestionLayout.java
@@ -1,7 +1,6 @@
-package view;
+package dev.blunch.blunch.view;
 
 import android.content.Context;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
