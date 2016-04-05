b81cfd7da19c2c06762484304bab7f612c84dff2
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-04-06 01:27:19 +0200

    syncronous methods to test in jUnit Firebase is not working properly in RoboElectric

--- app/src/main/java/dev/blunch/blunch/utils/FirebaseRepository.java
+++ app/src/main/java/dev/blunch/blunch/utils/FirebaseRepository.java
@@ -1,10 +1,17 @@
 package dev.blunch.blunch.utils;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.firebase.client.ValueEventListener;
+import com.firebase.client.FirebaseError;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Repository abstract class
@@ -12,10 +19,19 @@ import com.firebase.client.ValueEventListener;
  * @param <T> repository type
  * @author albert
  */
-public abstract class FirebaseRepository<T extends Entity> implements Repository <T> {
+public abstract class FirebaseRepository<T extends Entity> implements Repository<T>, ChildEventListener {
+
+    private OnChangedListener listener;
+
+    public interface OnChangedListener {
+        enum EventType {Added, Changed, Removed, Moved}
+
+        void onChanged(EventType type);
+    }
 
     protected final String FIREBASE_URI = "https://blunch.firebaseio.com/";
     private static final String TAG = FirebaseRepository.class.getSimpleName();
+    private final HashMap<String, T> map;
     protected Firebase firebase;
 
     /**
@@ -24,18 +40,14 @@ public abstract class FirebaseRepository<T extends Entity> implements Repository
     public FirebaseRepository(Context context) {
         Firebase.setAndroidContext(context);
         firebase = new Firebase(FIREBASE_URI);
-    }
+        map = new LinkedHashMap<>();
 
-    /**
-     * Add value Event listener for real time db
-     * @param listener value eventlistener
-     */
-    public void addValueEventListener(ValueEventListener listener) {
-        firebase.child(getObjectReference()).addValueEventListener(listener);
+        firebase.addChildEventListener(this);
     }
 
     /**
      * Insert some object in specific repository with random id.
+     *
      * @param item Object that you want to insert.
      */
     @Override
@@ -43,20 +55,24 @@ public abstract class FirebaseRepository<T extends Entity> implements Repository
         Firebase ref = firebase.child(getObjectReference()).push();
         ref.setValue(item);
         item.setId(ref.getKey());
+        map.put(ref.getKey(), item);
         return item;
     }
 
     /**
      * Insert some object in specific repository with specific id.
+     *
      * @param t   Object that you want to insert.
      * @param key Key that new object will have.
      */
     public void insertWithId(T t, String key) {
         firebase.child(getObjectReference()).child(key).setValue(t);
+        map.put(key, t);
     }
 
     /**
      * Delete some object of this repository by id.
+     *
      * @param id Object key that you want to delete.
      */
     public void delete(String id) {
@@ -66,6 +82,7 @@ public abstract class FirebaseRepository<T extends Entity> implements Repository
 
     /**
      * Delete a relation of an object of this repository.
+     *
      * @param idParent   Key of parent node.
      * @param reference  Reference of the relation.
      * @param idToDelete Key of node that you want to delete.
@@ -73,10 +90,12 @@ public abstract class FirebaseRepository<T extends Entity> implements Repository
     public void delete(String idParent, String reference, String idToDelete) {
         Firebase mRef = firebase.child(getObjectReference());
         mRef.child(idParent).child(reference).child(idToDelete).removeValue();
+
     }
 
     /**
      * Convert a Firebase attribute to Domain attribute.
+     *
      * @param data Firebase instance.
      * @return Domain instance.
      */
@@ -84,8 +103,50 @@ public abstract class FirebaseRepository<T extends Entity> implements Repository
 
     /**
      * Get object reference URI in Firebase.
+     *
      * @return Respective reference URI of object.
      */
     public abstract String getObjectReference();
 
+    public T get(String id) {
+        return map.get(id);
+    }
+
+    public List<T> all() {
+        return new ArrayList<>(map.values());
+    }
+
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+        map.put(dataSnapshot.getKey(), convert(dataSnapshot));
+        listener.onChanged(OnChangedListener.EventType.Added);
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        map.put(dataSnapshot.getKey(), convert(dataSnapshot));
+        listener.onChanged(OnChangedListener.EventType.Changed);
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+        map.remove(dataSnapshot.getKey());
+        listener.onChanged(OnChangedListener.EventType.Removed);
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        map.put(dataSnapshot.getKey(), convert(dataSnapshot));
+        listener.onChanged(OnChangedListener.EventType.Moved);
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+        Log.e(TAG, firebaseError.getMessage(), firebaseError.toException());
+    }
+
+    public void setOnChangedListener(OnChangedListener listener) {
+        this.listener = listener;
+    }
 }
--- app/src/test/java/dev/blunch/blunch/utils/FirebaseRepositoryTest.java
+++ app/src/test/java/dev/blunch/blunch/utils/FirebaseRepositoryTest.java
@@ -1,5 +1,7 @@
 package dev.blunch.blunch.utils;
 
+import junit.framework.AssertionFailedError;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +9,10 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowApplication;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import dev.blunch.blunch.BuildConfig;
 import dev.blunch.blunch.utils.dummy.Person;
@@ -33,8 +39,54 @@ public class FirebaseRepositoryTest {
     }
 
     @Test
-    public void add_correctly_some_object_in_repository(){
-        Person personToAdd = new Person("Albert", 20);
-        personRepository.insert(personToAdd);
+    public void add_correctly_some_object_in_repository() throws InterruptedException {
+        final Person personToAdd = new Person("Albert", 20);
+
+        runAndWaitUntil(personRepository, new Runnable() {
+            @Override
+            public void run() {
+                personRepository.insert(personToAdd);
+            }
+        }, new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return personRepository.all().size()==1;
+            }
+        });
+    }
+
+
+    public static void runAndWaitUntil(FirebaseRepository<Person> ref, Runnable task, Callable<Boolean> done) throws InterruptedException {
+        final java.util.concurrent.Semaphore semaphore = new java.util.concurrent.Semaphore(1);
+        semaphore.acquire();
+
+
+        FirebaseRepository.OnChangedListener listener = new FirebaseRepository.OnChangedListener() {
+            @Override
+            public void onChanged(EventType type) {
+                semaphore.release();
+            }
+        };
+
+
+        ref.setOnChangedListener(listener);
+        task.run();
+        boolean isDone = false;
+        boolean adquired = false;
+        long startedAt = System.currentTimeMillis();
+        while (!adquired && System.currentTimeMillis()-startedAt<5000) {
+            ShadowApplication.getInstance().getBackgroundThreadScheduler().runOneTask();
+            adquired = semaphore.tryAcquire(3, TimeUnit.SECONDS);
+            try {
+                isDone = done.call();
+            } catch (Exception e) {
+                e.printStackTrace();
+                // and we're not done
+            }
+        }
+        if (!isDone) {
+            throw new AssertionFailedError();
+        }
+        ref.setOnChangedListener(null);
     }
 }
