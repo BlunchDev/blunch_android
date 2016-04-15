9734eddefe61358239488e1d347c78d06df9e2e2
albert <albert.suarez.molgo@est.fib.upc.edu>
2016-04-15 15:20:43 +0200

    more tests

--- app/src/test/java/dev/blunch/blunch/activity/GetMenuCollaborativeActivityTest.java
+++ app/src/test/java/dev/blunch/blunch/activity/GetMenuCollaborativeActivityTest.java
@@ -1,6 +1,9 @@
 package dev.blunch.blunch.activity;
 
 import android.os.Build;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +13,9 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import dev.blunch.blunch.BuildConfig;
+import dev.blunch.blunch.R;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Get Menu Collaborative Activity Test
@@ -21,16 +27,47 @@ public class GetMenuCollaborativeActivityTest {
 
     GetMenuCollaborativeActivity activity;
 
+    private TextView    userName,
+                        localization,
+                        city,
+                        menuName,
+                        hostDishes,
+                        suggestions,
+                        description,
+                        hour;
+    private Button join;
+    private Toolbar toolbar;
+
     @Before
     public void setup() {
         activity = Robolectric.setupActivity(GetMenuCollaborativeActivity.class);
 
+        userName = (TextView) activity.findViewById(R.id.hostName);
+        localization = (TextView) activity.findViewById(R.id.hostLocalization);
+        city = (TextView) activity.findViewById(R.id.hostCity);
+        menuName = (TextView) activity.findViewById(R.id.menuName);
+        hostDishes = (TextView) activity.findViewById(R.id.hostDishes);
+        suggestions = (TextView) activity.findViewById(R.id.suggestions);
+        description = (TextView) activity.findViewById(R.id.description);
+        hour = (TextView) activity.findViewById(R.id.hour);
+        join = (Button) activity.findViewById(R.id.join);
+        toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
+        activity.setSupportActionBar(toolbar);
 
     }
 
     @Test
-    public void test() throws Exception {
-
+    public void shouldNotBeNull() throws Exception {
+        assertNotNull(userName);
+        assertNotNull(localization);
+        assertNotNull(city);
+        assertNotNull(menuName);
+        assertNotNull(hostDishes);
+        assertNotNull(suggestions);
+        assertNotNull(description);
+        assertNotNull(hour);
+        assertNotNull(join);
+        assertNotNull(toolbar);
     }
 
 }
