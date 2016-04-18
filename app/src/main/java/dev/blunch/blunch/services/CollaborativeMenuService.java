eb0d2639ac6b4b2d13c934c86b64fb92118360e3
jmotger <joaquim.motger@est.fib.upc.edu>
2016-04-18 17:12:39 +0200

    changed service and method trying to solve test bug

--- app/src/main/java/dev/blunch/blunch/services/CollaborativeMenuService.java
+++ app/src/main/java/dev/blunch/blunch/services/CollaborativeMenuService.java
@@ -58,12 +58,7 @@ public class CollaborativeMenuService extends Service<CollaborativeMenu> {
 
     public CollaborativeMenuAnswer reply(CollaborativeMenuAnswer collaborativeMenuAnswer,
                                          List<Dish> newOfferedDishes) throws Exception {
-        List<CollaborativeMenu> collaborativeMenus = repository.all();
-        boolean exists = false;
-        for (CollaborativeMenu menu : collaborativeMenus) {
-            if (menu.getId().equals(collaborativeMenuAnswer.getMenuId())) exists = true;
-        }
-        if (!exists) throw new Exception("El menu seleccionat no existeix");
+        if (!repository.exists(collaborativeMenuAnswer.getMenuId())) throw new Exception("El menu seleccionat no existeix");
         else if (collaborativeMenuAnswer.getOfferedDishes().size() == 0 && newOfferedDishes.size() == 0)
             throw new Exception("No s'han afegit plats a l'oferta de participació");
         for (Dish dish : newOfferedDishes) {
--- app/src/test/java/dev/blunch/blunch/activity/CollaborativeProposalAnswerActivityTest.java
+++ app/src/test/java/dev/blunch/blunch/activity/CollaborativeProposalAnswerActivityTest.java
@@ -1,41 +1,22 @@
 package dev.blunch.blunch.activity;
 
 import android.os.Build;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import junit.framework.AssertionFailedError;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowApplication;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 
 import dev.blunch.blunch.BuildConfig;
 import dev.blunch.blunch.R;
-import dev.blunch.blunch.domain.CollaborativeMenu;
-import dev.blunch.blunch.domain.CollaborativeMenuAnswer;
-import dev.blunch.blunch.domain.Dish;
-import dev.blunch.blunch.services.CollaborativeMenuService;
-import dev.blunch.blunch.utils.FirebaseRepository;
-import dev.blunch.blunch.utils.MockRepository;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
