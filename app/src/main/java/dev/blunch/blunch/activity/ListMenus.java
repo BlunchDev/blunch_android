600cf7a9f7b65922f364273b2c69c431ee385057
jmotger <joaquim.motger@est.fib.upc.edu>
2016-04-19 19:46:50 +0200

    small change in activity

--- app/src/main/java/dev/blunch/blunch/activity/ListMenus.java
+++ app/src/main/java/dev/blunch/blunch/activity/ListMenus.java
@@ -44,6 +44,13 @@ public class ListMenus extends AppCompatActivity {
         });
 
 
+        collaborativeMenuService.setOnChangedListener(new Repository.OnChangedListener() {
+            @Override
+            public void onChanged(EventType type) {
+                if (type.equals(EventType.Full))
+            }
+        });
+
     }
 
 }
