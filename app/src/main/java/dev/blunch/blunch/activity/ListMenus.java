505b01f0f818ff2cfc34a0f81cde861b7b42899c
jmotger <joaquim.motger@est.fib.upc.edu>
2016-04-19 13:54:24 +0200

    added activity & view for story 8

---
+++ app/src/main/java/dev/blunch/blunch/activity/ListMenus.java
@@ -0,0 +1,32 @@
+package dev.blunch.blunch.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import dev.blunch.blunch.R;
+
+public class ListMenus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_menus);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Ofertas de menú");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
