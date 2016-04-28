e5f860711e6e1728cec6c057d7343eedc4fb50a1
jmotger <joaquim.motger@est.fib.upc.edu>
2016-04-28 18:10:29 +0200

    refactorin of list menus activity into a navigational drawer

--- app/src/main/java/dev/blunch/blunch/activity/MainActivity.java
+++ app/src/main/java/dev/blunch/blunch/activity/MainActivity.java
@@ -5,9 +5,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -18,15 +25,14 @@ import java.util.List;
 
 import dev.blunch.blunch.R;
 import dev.blunch.blunch.adapters.MenuListAdapter;
-import dev.blunch.blunch.domain.Menu;
 import dev.blunch.blunch.services.CollaborativeMenuService;
 import dev.blunch.blunch.services.MenuService;
 import dev.blunch.blunch.services.PaymentMenuService;
 import dev.blunch.blunch.services.ServiceFactory;
 import dev.blunch.blunch.utils.Repository;
 
-@SuppressWarnings("all")
-public class ListMenusActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     MenuService menuService;
     CollaborativeMenuService collaborativeMenuService;
@@ -35,7 +41,7 @@ public class ListMenusActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_menus);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("BLUNCH");
@@ -52,6 +58,7 @@ public class ListMenusActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 init(parent.getItemAtPosition(position).toString());
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -67,18 +74,18 @@ public class ListMenusActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(ListMenusActivity.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setMessage("Qué tipo de menú quieres dar de alta?")
                         .setCancelable(false)
                         .setPositiveButton("Menú colaborativo", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                Intent intent = new Intent(ListMenusActivity.this, NewCollaborativeMenuActivity.class);
+                                Intent intent = new Intent(MainActivity.this, NewCollaborativeMenuActivity.class);
                                 startActivity(intent);
                             }
                         })
                         .setNegativeButton("Menú de pago", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                Intent intent = new Intent(ListMenusActivity.this, NewPaymentMenuActivity.class);
+                                Intent intent = new Intent(MainActivity.this, NewPaymentMenuActivity.class);
                                 startActivity(intent);
                             }
                         });
@@ -91,15 +98,24 @@ public class ListMenusActivity extends AppCompatActivity {
             @Override
             public void onChanged(EventType type) {
                 if (type.equals(EventType.Full)) {
-                    init("All");
+                   init("All");
                 }
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void init(String filter) {
 
-        List<Menu> menuList = new ArrayList<>();
+        List<dev.blunch.blunch.domain.Menu> menuList = new ArrayList<>();
 
         switch (filter) {
             case "Todos":
@@ -126,18 +142,18 @@ public class ListMenusActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Menu menu = menuListAdapter.getItem(position);
+                dev.blunch.blunch.domain.Menu menu = menuListAdapter.getItem(position);
 
                 String s = menu.getClass().getSimpleName();
 
                 switch (s) {
                     case "CollaborativeMenu":
-                        Intent intent = new Intent(ListMenusActivity.this, GetCollaborativeMenuActivity.class);
+                        Intent intent = new Intent(MainActivity.this, GetCollaborativeMenuActivity.class);
                         intent.putExtra(GetCollaborativeMenuActivity.MENU_ID_KEY, menu.getId());
                         startActivity(intent);
                         break;
                     case "PaymentMenu":
-                        Intent intent2 = new Intent(ListMenusActivity.this, GetPaymentMenuActivity.class);
+                        Intent intent2 = new Intent(MainActivity.this, GetPaymentMenuActivity.class);
                         intent2.putExtra(GetPaymentMenuActivity.MENU_ID_KEY, menu.getId());
                         startActivity(intent2);
                         break;
@@ -147,14 +163,56 @@ public class ListMenusActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        finish();
-        startActivity(getIntent());
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.my_menus) {
+
+        } else if (id == R.id.collaborating_menus) {
+
+        } else if (id == R.id.payment_menus) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
