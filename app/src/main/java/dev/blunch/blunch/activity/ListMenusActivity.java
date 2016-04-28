13bac41153332493692f954624324d3322f5887d
jmotger <joaquim.motger@est.fib.upc.edu>
2016-04-28 12:04:59 +0200

    layout color and icons changed

--- app/src/main/java/dev/blunch/blunch/activity/ListMenusActivity.java
+++ app/src/main/java/dev/blunch/blunch/activity/ListMenusActivity.java
@@ -71,6 +71,7 @@ public class ListMenusActivity extends AppCompatActivity {
         paymentMenuService = ServiceFactory.getPaymentMenuService(getApplicationContext());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
