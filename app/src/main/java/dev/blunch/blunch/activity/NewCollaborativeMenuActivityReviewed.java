14e0ee5cb627f52d7bb50de304e8da930545f01c
jmotger <joaquim.motger@est.fib.upc.edu>
2016-04-28 15:47:33 +0200

    finished refactoring of new collaborative menu activity

--- app/src/main/java/dev/blunch/blunch/activity/NewCollaborativeMenuActivityReviewed.java
+++ app/src/main/java/dev/blunch/blunch/activity/NewCollaborativeMenuActivityReviewed.java
@@ -26,6 +26,7 @@ import dev.blunch.blunch.R;
 import dev.blunch.blunch.domain.CollaborativeMenu;
 import dev.blunch.blunch.domain.Dish;
 import dev.blunch.blunch.services.CollaborativeMenuService;
+import dev.blunch.blunch.services.ServiceFactory;
 import dev.blunch.blunch.view.CollaborativeDishLayout;
 
 @SuppressWarnings("all")
@@ -42,6 +43,7 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
     private Date start,
             finish;
     protected ArrayList<CollaborativeDishLayout> myDishes = new ArrayList<>();
+    protected ArrayList<CollaborativeDishLayout> suggestedDishes = new ArrayList<>();
 
     private CollaborativeMenuService collaborativeMenuService;
 
@@ -52,6 +54,7 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        collaborativeMenuService = ServiceFactory.getCollaborativeMenuService(getApplicationContext());
         setTitle("Crear menú colaborativo");
         initialize();
 
@@ -70,8 +73,10 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
 
         final EditText menuName = (EditText) findViewById(R.id.nomMenu);
         final ImageButton moreDishes = (ImageButton) findViewById(R.id.moreDishes);
+        final ImageButton moreSuggestedDishes = (ImageButton) findViewById(R.id.moreSuggestdDishes);
         final LinearLayout moreDishesLayout = (LinearLayout) findViewById(R.id.dishesLayout);
-        final CollaborativeDishLayout menu = new CollaborativeDishLayout(getApplicationContext());
+        final LinearLayout moreSuggestedDishesLayout = (LinearLayout) findViewById(R.id.suggestedDishesLayout);
+        /*final CollaborativeDishLayout menu = new CollaborativeDishLayout(getApplicationContext());
         myDishes.add(menu);
         moreDishesLayout.addView(menu);
         menu.getClose().setOnClickListener(new View.OnClickListener() {
@@ -82,7 +87,7 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
                     myDishes.remove(menu);
                 }
             }
-        });
+        });*/
 
         moreDishes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,10 +99,25 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
                 close.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (myDishes.size() > 1) {
-                            moreDishesLayout.removeView(a);
-                            myDishes.remove(a);
-                        }
+                        moreDishesLayout.removeView(a);
+                        myDishes.remove(a);
+                    }
+                });
+            }
+        });
+
+        moreSuggestedDishes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final CollaborativeDishLayout a = new CollaborativeDishLayout(NewCollaborativeMenuActivityReviewed.this);
+                suggestedDishes.add(a);
+                moreSuggestedDishesLayout.addView(a);
+                ImageButton close = a.getClose();
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        moreSuggestedDishesLayout.removeView(a);
+                        suggestedDishes.remove(a);
                     }
                 });
             }
@@ -151,7 +171,7 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
         start = cStart.getTime();
 
         String dayS = String.valueOf(this.day);
-        String monthS = String.valueOf(this.month);
+        String monthS = String.valueOf(this.month + 1);
         String yearS = String.valueOf(this.year);
         if (this.day < 10) dayS = "0" + dayS;
         if (this.month < 10) monthS = "0" + monthS;
@@ -268,22 +288,33 @@ public class NewCollaborativeMenuActivityReviewed extends AppCompatActivity {
             for (CollaborativeDishLayout dishLayout : myDishes) {
                 if (!dishLayout.getDishName().equals("")) {
                     Dish dish = new Dish(dishLayout.getDishName());
-                    if (!dishLayout.isSuggest()) offeredDish.add(dish);
-                    else suggestedDish.add(dish);
+                    offeredDish.add(dish);
                 }
             }
 
+            for (CollaborativeDishLayout dishLayout : suggestedDishes) {
+                if (!dishLayout.getDishName().equals("")) {
+                    Dish dish = new Dish(dishLayout.getDishName());
+                    suggestedDish.add(dish);
+                }
+            }
 
-            CollaborativeMenu collaborativeMenu = new CollaborativeMenu(menuNameString,
-                    author,
-                    description,
-                    localization,
-                    start,
-                    finish);
-            collaborativeMenuService.save(collaborativeMenu, offeredDish, suggestedDish);
-            Toast.makeText(this, "Menu colaborativo creado correctamente!",
-                    Toast.LENGTH_LONG).show();
-            finish();
+            if (offeredDish.size() == 0) {
+                Toast.makeText(getApplicationContext(), "Debes ofrecer al menos un plato!", Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                CollaborativeMenu collaborativeMenu = new CollaborativeMenu(menuNameString,
+                        author,
+                        description,
+                        localization,
+                        start,
+                        finish);
+                collaborativeMenuService.save(collaborativeMenu, offeredDish, suggestedDish);
+                Toast.makeText(this, "Menú colaborativo creado correctamente!",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
         }
     }
 
--- app/src/main/java/dev/blunch/blunch/view/CollaborativeDishLayout.java
+++ app/src/main/java/dev/blunch/blunch/view/CollaborativeDishLayout.java
@@ -23,13 +23,13 @@ import dev.blunch.blunch.R;
 public class CollaborativeDishLayout extends LinearLayout{
 
     private EditText dishName;
-    private Switch switch1;
     private ImageButton close;
 
 
     public CollaborativeDishLayout(Context context) {
         super(context);
         setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER);
         setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
         Space space1 = new Space(context);
@@ -42,6 +42,7 @@ public class CollaborativeDishLayout extends LinearLayout{
         dishName.setHintTextColor(getResources().getColor(R.color.gray));
         dishName.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
         dishName.setHint("Nombre");
+        dishName.setWidth(10);
         LinearLayout.LayoutParams nomPlatLayout = new LinearLayout.LayoutParams(350, ViewGroup.LayoutParams.WRAP_CONTENT);
 
         addView(dishName, nomPlatLayout);
@@ -51,7 +52,7 @@ public class CollaborativeDishLayout extends LinearLayout{
         addView(space5, space5Layout);
 
         close = new ImageButton(context);
-        close.setBackgroundColor(getResources().getColor(R.color.background));
+        close.setBackgroundColor(getResources().getColor(R.color.white));
         close.setImageResource(R.drawable.cross);
         close.setScaleType(ImageView.ScaleType.FIT_XY);
         close.setScaleX(0.5f);
@@ -71,38 +72,10 @@ public class CollaborativeDishLayout extends LinearLayout{
         LinearLayout.LayoutParams space6Layout = new LinearLayout.LayoutParams(15, ViewGroup.LayoutParams.MATCH_PARENT);
         addView(space6, space6Layout);
 
-        TextView me = new TextView(context);
-        me.setText("Yo");
-        me.setTextColor(getResources().getColor(R.color.gray));
-        LinearLayout.LayoutParams meLayout = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        addView(me, meLayout);
-
-        Space space2 = new Space(context);
-        LinearLayout.LayoutParams space2Layout = new LinearLayout.LayoutParams(25, ViewGroup.LayoutParams.MATCH_PARENT);
-        addView(space2, space2Layout);
-
-        switch1 = new Switch(context);
-        switch1.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        switch1.setChecked(false);
-        LinearLayout.LayoutParams switchLayout = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        addView(switch1, switchLayout);
-
-        Space space3 = new Space(context);
-        LinearLayout.LayoutParams space3Layout = new LinearLayout.LayoutParams(25, ViewGroup.LayoutParams.MATCH_PARENT);
-        addView(space3, space3Layout);
-
-        TextView sug = new TextView(context);
-        sug.setText("Sug");
-        sug.setTextColor(getResources().getColor(R.color.gray));
-        addView(sug, meLayout);
     }
 
     public String getDishName(){
         return dishName.getText().toString();
-    }
-
-    public Boolean isSuggest() {
-        return switch1.isChecked();
     }
 
     public ImageButton getClose() {
