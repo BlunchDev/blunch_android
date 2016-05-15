8b3f73c07dd632a0f976c2d80e465bcd0b25afdf
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-05-15 13:18:59 +0200

    add location request dialog

--- app/src/main/java/dev/blunch/blunch/activity/MenusLocationActivity.java
+++ app/src/main/java/dev/blunch/blunch/activity/MenusLocationActivity.java
@@ -4,15 +4,11 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,7 +20,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -34,13 +31,13 @@ import dev.blunch.blunch.domain.PaymentMenu;
 import dev.blunch.blunch.services.MenuService;
 import dev.blunch.blunch.services.ServiceFactory;
 import dev.blunch.blunch.utils.IntentUtils;
+import dev.blunch.blunch.utils.Utils;
 
 public class MenusLocationActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnInfoWindowClickListener,GoogleMap.OnMarkerClickListener {
+        GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
-    Location localizacion;
-    MenuService menuService;
+    private MenuService menuService;
 
 
     @Override
@@ -51,10 +48,9 @@ public class MenusLocationActivity extends FragmentActivity implements OnMapRead
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        localizacion = null;
         menuService = ServiceFactory.getMenuService(getApplicationContext());
     }
-    
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -69,30 +65,31 @@ public class MenusLocationActivity extends FragmentActivity implements OnMapRead
             // Show rationale and request permission.
         }
 
-            //we search for the fib
-            LatLng BCN = getLocationFromAddress("Barcelona");
-            if(BCN != null) {
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(BCN)      // Sets the center to DA FIB
-                        .zoom(10)         // we set the zoom
-                        .build();
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),5000,null);
-            }
-            situaMenus();
+        //we search for the fib
+        LatLng BCN = getLocationFromAddress("Barcelona");
+        if (BCN != null) {
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(BCN)      // Sets the center to DA FIB
+                    .zoom(10)         // we set the zoom
+                    .build();
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000, null);
         }
-//    }
+        situaMenus();
+    }
 
-    public void situaMenus(){
+    public void situaMenus() {
         List<Menu> menus = menuService.getMenus();
-        for(Menu m : menus){
-            if(m.getDateEnd().after(new Date())) {
+        for (Menu m : menus) {
+            if (m.getDateEnd().after(new Date())) {
                 LatLng posicion = getLocationFromAddress(m.getLocalization());
                 if (posicion != null) {
+                    DateFormat sdf = SimpleDateFormat.getDateTimeInstance();
                     MarkerOptions marcador = new MarkerOptions()
                             .position(posicion)
                             .title(m.getId())
-                            .snippet("Finalización: " + m.getDateEnd());
-                    if (m instanceof PaymentMenu) marcador.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                            .snippet("Finalización: " + sdf.format(m.getDateEnd()));
+                    if (m instanceof PaymentMenu)
+                        marcador.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
                     mMap.addMarker(marcador);
                 }
             }
@@ -100,25 +97,7 @@ public class MenusLocationActivity extends FragmentActivity implements OnMapRead
     }
 
     public LatLng getLocationFromAddress(String strAddress) {
-
-        Geocoder coder = new Geocoder(this);
-        List<Address> address = null;
-        LatLng coordenades = null;
-        try {
-            try {
-                address = coder.getFromLocationName(strAddress, 5);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (address == null) {
-                return null;
-            }
-            Address location = address.get(0);
-            coordenades = new LatLng(location.getLatitude(), location.getLongitude());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return coordenades;
+        return Utils.getLocationFromAddress(strAddress, getApplicationContext());
     }
 
     private Boolean gpsEnabled() {
@@ -126,15 +105,14 @@ public class MenusLocationActivity extends FragmentActivity implements OnMapRead
                 .getContentResolver();
         boolean gpsStatus = Settings.Secure
                 .isLocationProviderEnabled(contentResolver,
-                LocationManager.GPS_PROVIDER);
-         return gpsStatus;
+                        LocationManager.GPS_PROVIDER);
+        return gpsStatus;
     }
 
     @Override
     public boolean onMyLocationButtonClick() {
-        if(gpsEnabled()) return false;
-        Toast.makeText(this, "GPS no conectado",
-                Toast.LENGTH_LONG).show();
+        if (gpsEnabled()) return false;
+        Utils.showLocationDialog(getApplicationContext());
         return false;
     }
 
@@ -142,7 +120,7 @@ public class MenusLocationActivity extends FragmentActivity implements OnMapRead
     @Override
     public void onInfoWindowClick(Marker marker) {
         Menu m = menuService.getMenu(marker.getTitle());
-        Intent intent = IntentUtils.getMenuDetailIntent(m,getApplicationContext());
+        Intent intent = IntentUtils.getMenuDetailIntent(m, getApplicationContext());
         intent.putExtra("menuId", m.getId());
         startActivity(intent);
     }
@@ -150,7 +128,7 @@ public class MenusLocationActivity extends FragmentActivity implements OnMapRead
     @Override
     public boolean onMarkerClick(Marker marker) {
         Menu m = menuService.getMenu(marker.getTitle());
-        Intent intent = IntentUtils.getMenuDetailIntent(m,getApplicationContext());
+        Intent intent = IntentUtils.getMenuDetailIntent(m, getApplicationContext());
         intent.putExtra("menuId", m.getId());
         startActivity(intent);
         return true;
--- app/src/main/java/dev/blunch/blunch/utils/Utils.java
+++ app/src/main/java/dev/blunch/blunch/utils/Utils.java
@@ -1,6 +1,19 @@
 package dev.blunch.blunch.utils;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+
 import com.firebase.client.Firebase;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+import dev.blunch.blunch.R;
 
 /**
  * Utils class
@@ -10,6 +23,43 @@ public class Utils {
 
     public static String generateId() {
         return new Firebase("https://blunch.firebaseio.com/").push().getKey();
+    }
+
+    public static void showLocationDialog(final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.location_request_title);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent settings = new Intent("com.google.android.gms.location.settings.GOOGLE_LOCATION_SETTINGS");
+                context.startActivity(settings);
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.setMessage(R.string.location_request_message);
+        builder.create().show();
+    }
+
+    public static LatLng getLocationFromAddress(String address, Context context) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> results = null;
+        LatLng coordenades = null;
+        try {
+            try {
+                results = coder.getFromLocationName(address, 5);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (results == null) {
+                return null;
+            }
+            Address location = results.get(0);
+            coordenades = new LatLng(location.getLatitude(), location.getLongitude());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return coordenades;
     }
 
 }
