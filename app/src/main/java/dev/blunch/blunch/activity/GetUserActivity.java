6a7dfd0988c796ce5b0731ec9200bfee42891adb
Pere Berge <pere.berge@est.fib.upc.edu>
2016-05-27 18:58:25 +0200

    some errors fixed

--- app/src/main/java/dev/blunch/blunch/activity/GetUserActivity.java
+++ app/src/main/java/dev/blunch/blunch/activity/GetUserActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -20,6 +22,8 @@ public class GetUserActivity extends AppCompatActivity {
     private ImageView userImage;
     private TextView userName;
     private RatingBar userValoration;
+    private Button myMenus;
+    private Button myValorations;
 
     public static final String USER_ID = "userId";
 
@@ -41,18 +45,23 @@ public class GetUserActivity extends AppCompatActivity {
         userImage = (ImageView) findViewById(R.id.user_picture_get);
         userName = (TextView) findViewById(R.id.user_name_get);
         userValoration = (RatingBar) findViewById(R.id.user_rating_get);
+        myMenus = (Button) findViewById(R.id.myMenus);
+        myValorations = (Button) findViewById(R.id.myValorations);
+
+
         menuService = ServiceFactory.getMenuService(getApplicationContext());
 
         Intent intent = getIntent();
-        if(intent.hasExtra(USER_ID))
-        userId = intent.getStringExtra(USER_ID);
+        if(intent.hasExtra(USER_ID)) {
+            this.userId = intent.getStringExtra(USER_ID);
+        }
 
-        User user = menuService.findUserByEmail(getIntent().getStringExtra(userId));
+        User user = menuService.findUserByEmail(this.userId);
 
         userImage.setImageDrawable(getUserPic());
         userName.setText(user.getName());
         userValoration.setRating((float) user.getValorationAverage());
-        userValoration.setOnClickListener(new View.OnClickListener() {
+        myValorations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(GetUserActivity.this, ValorationListActivity.class);
@@ -60,11 +69,12 @@ public class GetUserActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
     private Drawable getUserPic() {
         try {
-            return menuService.findUserByEmail(getIntent().getStringExtra(userId)).getImageRounded(getResources());
+            return menuService.findUserByEmail(this.userId).getImageRounded(getResources());
         } catch (Exception e) {
             e.printStackTrace();
         }
